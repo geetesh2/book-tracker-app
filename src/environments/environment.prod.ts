a15e7923dbc7859a15f1cdcae31f355c4bffe7cc
Geetesh <geeteshpandey2@gmail.com>
2025-04-16 20:41:15 +0530

    Add Prod Environment file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    booksApiKey: 'AIzaSyDZo98mIafQbKHHVhBmMTUKzP07VWsGg1M',
+    firebaseApikey: 'AIzaSyDuM3RSW2cYftlom8mSbAG2MaCZjTcIx5o'
+  };
+  
